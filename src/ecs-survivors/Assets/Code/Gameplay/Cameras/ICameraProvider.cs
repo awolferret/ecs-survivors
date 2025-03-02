88a6e379df319aca99b4cc8627f09e24b694a364
awolferret <104518827+awolferret@users.noreply.github.com>
2025-03-03 00:03:31 +0300

    resouces

--- src/ecs-survivors/Assets/Code/Gameplay/Cameras/ICameraProvider.cs
+++ src/ecs-survivors/Assets/Code/Gameplay/Cameras/ICameraProvider.cs
@@ -1,10 +1,12 @@
+using UnityEngine;
+
 namespace Code.Gameplay.Cameras
 {
   public interface ICameraProvider
   {
-    UnityEngine.Camera MainCamera { get; }
+    Camera MainCamera { get; }
     float WorldScreenHeight { get; }
     float WorldScreenWidth { get; }
-    void SetMainCamera(UnityEngine.Camera camera);
+    void SetMainCamera(Camera camera);
   }
 }
